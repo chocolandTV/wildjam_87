a93578a92d90933e118517aa31987acf730bc39f
Robert Moegenburg <r.moegenburg@t-online.de>
2025-11-15 16:25:23 +0100

    canvas_info

---
+++ ui/Interactive_info/canvas_info.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name Canvas_Info
+# Canvas information properties
+
+@export var canvas_name : String = "Default Canvas"
+@export var canvas_description : String = "This is a default canvas description."
+@export var canvas_text_01 : String = "Gravity: 9.8 m/s²"
+@export var canvas_text_02 : String = "Atmosphere: Nitrogen-Oxygen"
+@export var canvas_text_03 : String = "Habitability:  High"
+@export var canvas_text_04 : String = "Distance from Star: 150 million km"
+@export var canvas_text_05 : String = "Orbital Period: 365 days"
+@export var canvas_text_06 : String = "Surface Temperature: 15°C"
+
+@export var canvas_width : int = 800
+@export var canvas_height : int = 600
+@export var background_color : Color = Color(0, 0, 0)
+@export var canvas_background_texture : Texture2D
+
