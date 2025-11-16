f2018a397b0ddb2c4ca4120f3b30f0fe42054b4b
Robert Moegenburg <r.moegenburg@t-online.de>
2025-11-16 09:10:04 +0100

    cycle_manager

--- entities/Cylce/cycle_manager.gd
+++ entities/Cylce/cycle_manager.gd
@@ -3,7 +3,7 @@ extends Node
 signal cycle_changed(cycle_stored_upgrades : Dictionary)
 
 var current_cycle :int = 0
-var current_lifetime : int = 60
+var current_lifetime : float = 60
 
 var cycle_stored_upgrades : Dictionary = {
     "upgrade_lifetime" : 0,
@@ -11,9 +11,15 @@ var cycle_stored_upgrades : Dictionary = {
     "upgrade_scan_efficiency" : 0,
     "upgrade_mining_efficiency" : 0
 }
+func _physics_process(delta: float) -> void:
+    if current_lifetime > 0:
+        current_lifetime -= delta
+    else:
+        next_cycle()
 ############################################ PUBLIC METHODS ############################################
 func next_cycle() -> void:
     current_cycle += 1
+    print("CycleManager: Advancing to next cycle.Now on cycle %d" % current_cycle)
     cycle_changed.emit(cycle_stored_upgrades)
     # give new upgrade to the player
     #increase lifetime upgrade by 1
@@ -24,3 +30,5 @@ func next_cycle() -> void:
         "upgrade_scan_efficiency" : 0,
         "upgrade_mining_efficiency" : 0
     }
+    #Reset lifetime for next cycle
+    current_lifetime =  60 + PersistentData.player_progress.get("upgrade_lifetime", 1)
